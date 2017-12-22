9351133df7f4e18e48ec2e8a2482a2abe2e14902
Sebastian Muhle <SebastianMuhle@me.com>
2017-12-22 20:15:10 +0100

    First draft hyperparameter tuning

--- TrainingCNN.py
+++ TrainingCNN.py
@@ -21,17 +21,30 @@ img.show()
 # Do to: Do following in a grid search  for hyper parameter tuning
 
 # Hyperparameters
-num_freezedLayers=249
+num_freezedLayersArray=[5, 80, 249]
+learning_rates=[0.01, 0.001, 0.0001]
 
-# Create optimizer
-optimizer = tf.keras.optimizers.SGD(lr=0.0001, momentum=0.9)
+# Hyperparameter search
+for num_freezedLayers in num_freezedLayersArray:
+    for lr in learning_rates:
 
-# Create model
-model = InceptionV3model().create_model(num_freezedLayers=num_freezedLayers, optimizer=optimizer)
-estimator_model = tf.keras.estimator.model_to_estimator(keras_model= model)
+        # Create Optimizer
+        optimizerSGD = tf.keras.optimizers.SGD(lr=0.0001, momentum=0.9)
+        optimizerAdam = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0,)
 
-# Training
-train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=1000)
-eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
+        # Create model
+        model = InceptionV3model().create_model(num_freezedLayers=num_freezedLayers, optimizer=optimizerSGD)
+        estimator_model = tf.keras.estimator.model_to_estimator(keras_model= model)
 
-tf.estimator.train_and_evaluate(estimator_model, train_spec, eval_spec)
+        # Training
+        train_spec = tf.estimator.TrainSpec(input_fn=lambda: imgs_input_fn(test_files,
+                                                                           labels=test_labels,
+                                                                           perform_shuffle=True,
+                                                                           repeat_count=5,
+                                                                           batch_size=20), max_steps=500)
+        eval_spec = tf.estimator.EvalSpec(input_fn=lambda: imgs_input_fn(test_files,
+                                                                         labels=test_labels,
+                                                                         perform_shuffle=False,
+                                                                         batch_size=1))
+
+        tf.estimator.train_and_evaluate(estimator_model, train_spec, eval_spec)
