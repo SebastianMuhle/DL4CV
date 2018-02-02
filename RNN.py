4f3ae1f8b20117a45fddbb146977c43589a72f8c
Haydar Sahin <haydarsahin93@gmail.com>
2018-02-02 01:52:38 +0100

    memory management

--- RNN.py
+++ RNN.py
@@ -21,24 +21,26 @@ print("Total Vocab: ", n_vocab)
 
 # prepare the dataset of input to output pairs encoded as integers
 seq_length = 100 # maybe change it
-dataX = []
-dataY = []
-print("Loops: ",n_chars - seq_length)
-for i in range(0, n_chars - seq_length, 1):
+X = []
+y = []
+loop_count = n_chars - seq_length
+loop_count = 10000000
+print("Loops: ",loop_count)
+for i in range(0, loop_count, 1):
 	print(i)
 	seq_in = raw_text[i:i + seq_length]
 	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
+	X.append([char_to_int[char] for char in seq_in])
+	y.append(char_to_int[seq_out])
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 
 # reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+X = numpy.reshape(numpy.array(X), (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable
-y = np_utils.to_categorical(dataY)
+y = np_utils.to_categorical(y)
 
 # Hyperparameters
 num_layers = [2, 4, 6]
